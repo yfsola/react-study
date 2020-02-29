cd36e1597ffb8d2d5ed0586249259aba26fabbd0
solayf <m15500104512@163.com>
2020-02-29 20:03:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button from 'antd/es/button'
+import 'antd/dist/antd.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app<Button type="primary">primary</Button>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,42 +2,10 @@
 import React from 'react';
 // 渲染真实dom vdom - > dom
 import ReactDOM from 'react-dom';
-// import './index.css';
-import style from './index.module.css'
-import logo from './logo.svg'
-const obj = {
-  firstName: 'hurry',
-  lastName: 'pottry'
-}
-function formatName(name) {
-  return name.firstName + ' ' + name.lastName
-}
-const greed = <div>jsx对象</div>
-const arr = [0,1,2]
-const show = true
-const name = 'react'
-const jsx = (
-  <div className={style.app}>
-
-    
-    <div>hello ,{name}</div>
-    <div>{formatName(obj)}</div>
-    {greed}
-    {show?greed:'不显示'}
-    {show && greed}
-    <ul>
-      {arr.map(item => <li key={item}>{item}</li>)}
-    </ul>
-
-
-    <img src={logo} className={style.logo}/>
-
-
-  </div>
-);
+import App from './App'
 
 // 渲染页面，将引入的APP页面插入到ID为root的元素中
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // 基本使用 ，表达式用{}包裹
 // 函数的使用
